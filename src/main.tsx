b474f4378330892c5b40bb62251e0e0998887835
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-01-19 23:12:58 +0530

    feat: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient';
 import App from './App';
 import './index.css';
 import { initTheme } from './lib/theme';
@@ -10,8 +12,10 @@ initTheme();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
