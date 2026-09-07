417972722d564810430ce89f1d374a808397f094
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-09-07 09:58:57 +0530

    chore: update backend/internal/models/timer.go (12/57)

--- backend/internal/models/timer.go
+++ backend/internal/models/timer.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Session struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	UserID    primitive.ObjectID `bson:"userId" json:"userId"`
-	Duration  int                `bson:"duration" json:"duration"` // in minutes
-	Subject   string             `bson:"subject" json:"subject"`
-	StartTime time.Time          `bson:"startTime" json:"startTime"`
-	EndTime   time.Time          `bson:"endTime" json:"endTime"`
-	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserID           primitive.ObjectID `bson:"userId" json:"userId"`
+	Duration         int                `bson:"duration" json:"duration"` // in minutes
+	Subject          string             `bson:"subject" json:"subject"`
+	StartTime        time.Time          `bson:"startTime" json:"startTime"`
+	EndTime          time.Time          `bson:"endTime" json:"endTime"`
+	CreatedAt        time.Time          `bson:"createdAt" json:"createdAt"`
+	ClientMutationId string             `bson:"clientMutationId,omitempty" json:"clientMutationId,omitempty"`
 }
